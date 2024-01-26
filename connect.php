4e59a4a73d5e8726d658cff5176eac16625938d1
KENNEDY_MATIVO <150677178+KEN-BD@users.noreply.github.com>
2024-01-26 19:43:27 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
+ $conn=mysqli_connect('localhost','root','!Kennmativo9402','test') or die("conection faled!" .mysqli_connect_error());
+ if(isset($_POST['name']) &&isset($_POST['email']) &&isset($_POST['phone']) &&isset($_POST['bgroup']))
+  {
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+    $bgroup=$_POST['bgroup'];
+
+    $sql = "INSERT INTO `users` (`name`, `email`, `phone`, `bgroup`) VALUES ('$name', '$email', '$phone', '$bgroup')";
+
+ $query = mysqli_query($conn, $sql);
+    if($query){
+    echo 'Entry Successful';
+    }
+    else{
+    echo 'Error Occured' .mysqli_error($conn);
+    }
+ }
+}
+?>
